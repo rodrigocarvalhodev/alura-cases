245612a4888ec5349cb1ed984a1b00949c779b8e
Rodrigo Carvalho <rodrigo.carvalho@codescript.com.br>
2023-03-24 15:54:55 -0300

    :sparkles: test
    test

---
+++ pages/faq.js
@@ -0,0 +1,12 @@
+import Link from 'next/link'
+
+export default function FAQPage() {
+    return (
+        <div>
+            <h1>Alura Cases FAQ</h1>
+            <Link href="/">
+                Ir para a página principal.
+            </Link>
+        </div>
+    )
+}
---
+++ pages/index.js
@@ -0,0 +1,12 @@
+import Link from 'next/link'
+
+export default function HomePage() {
+    return (
+        <div>
+            <h1>Alura Cases</h1>
+            <Link href="faq">
+                Ir para o FAQ
+            </Link>
+        </div>
+    )
+}
